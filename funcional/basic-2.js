a40d2455ca2f08ea43dd808c733d8608e1d8b359
Lucas Fernando <lucas98fernando@gmail.com>
2021-07-20 10:27:15 -0300

    Aprendendo mais um pouco...

---
+++ funcional/basic-2.js
@@ -0,0 +1,31 @@
+function bomDia() {
+    console.log("Bom dia !")
+}
+
+let boaTarde = function() {
+    console.log("Boa tarde !")
+}
+
+// Passando uma função para outra função
+function executaAlgumaCoisa(fn) {
+    if (typeof fn === 'function') {
+        fn()
+    } else {
+        console.log("Não é uma função")
+    }
+}
+
+executaAlgumaCoisa(bomDia)
+executaAlgumaCoisa(boaTarde)
+executaAlgumaCoisa(45)
+
+// Retornar uma função a partir de uma outra
+function potencia(base) {
+    return function(exp) {
+        return Math.pow(base, exp)
+    }
+}
+
+const potenciaDe2 = potencia(2)
+console.log(potenciaDe2(8))
+console.log(potencia())
