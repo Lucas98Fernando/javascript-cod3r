fbfe68f21bf059297c4d83298faa6d2087daea57
Lucas Fernando <lucas98fernando@gmail.com>
2021-08-03 15:22:13 -0300

    Implementa um fluxo de como funciona o reduce

--- funcional/callback-5.js
+++ funcional/callback-5.js
@@ -41,3 +41,23 @@ const getValoresCarrinho = item => item.qtd * item.preco
 const totalCarrinho = (total, item) => total + item
 
 console.log(carrinho.filter(itemsValidos).map(getValoresCarrinho).reduce(totalCarrinho))
+
+Array.prototype.meuReduce = function (fn, inicial) {
+    // Caso tenha um valor inicial, o acumulador irá recebé-lo
+    let acumulador = inicial
+    for (let i = 0; i < this.length; i++) {
+        // Se não houver valor inicial e o indíce do array for 0, o acumulador vai receber o primeiro elemento do array
+        if (!acumulador && i === 0) {
+            acumulador = this[i]
+        }
+        // Se não, o acumulador recebe uma função
+        else {
+            acumulador = fn(acumulador, this[i], i, this)
+        }
+    }
+    // Retorna o valor final após de varrer todo o array
+    return acumulador
+}
+
+// Função com o reduce personalizado
+console.log(carrinho.filter(itemsValidos).map(getValoresCarrinho).meuReduce(totalCarrinho))
